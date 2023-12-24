121635abd08f2735bf3d4b53dd2df5d0a332b947
Ian Wafula <neocorpse@proton.me>
2023-12-24 21:58:24 +0300

    Add simple UI for game interface
    Used DOM Manipulation properties and event Listeners for buttons to
    control the game.

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 "use strict";
 
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
 const allowed = ['rock', 'paper', 'scissors']
+const button = document.querySelectorAll('button')
+const status = document.querySelector('.status')
+const lastRound = document.querySelector('.last')
+const theEnd = document.querySelector('.container');
+let wins = 0, losses = 0, ties = 0, rounds = 0;
+let x;
+
+const round = (e)=> {
+    x = playRound(e.target.textContent.toLowerCase(),getComputerChoice());
+    if (x.includes('win')) {
+        wins++;
+        rounds++;
+    } else if(x.includes('lose')) {
+        losses++;
+        rounds++;
+    } else if (x.includes('tie')) {
+        ties++;
+    }
+
+    if (wins == 5) {
+        endGame('win');
+    } else if (losses == 5) {
+        endGame('lose')
+    }
+
+    console.log(x)
+
+    lastRound.textContent = x
+    status.textContent = `Wins: ${wins}  Losses: ${losses}  Ties: ${ties}`
+}
+
+
+button.forEach((value) => {
+    value.addEventListener('click', round)
+})
 
 function getComputerChoice() {
     const random = Math.ceil(Math.random() * 3 );
@@ -41,26 +79,58 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
-function game() {
-    let choice;
-    let wins = 0, losses = 0, ties = 0;
-    let rounds = 0;
-    let x;
-    while (rounds < 5) {
-        choice = prompt('Choose between rock, paper and scissors.').toLowerCase()
-        while (!(allowed.includes(choice))) {
-            choice = prompt(`Please select only the allowed values, that is: ${allowed}`).toLowerCase();
-        }
-        console.log(x = playRound(choice, getComputerChoice()))
-        if (x.includes('win')) {
-            wins++;
-            rounds++;
-        } else if(x.includes('lose')) {
-            losses++;
-            rounds++;
-        } else if (x.includes('tie')) {
-            ties++;
-        }
+function endGame(result) {
+    if (result == 'win') {
+        theEnd.textContent += `Yaay! You won with ${wins} points.`
+    } else if (result == 'lose') {
+        theEnd.textContent += `Bummer! You lost with ${losses} points.`
     }
-    (wins > losses) ? console.log(`Yaaay!! You won with ${wins} wins, ${losses} losses and ${ties} ties.`) : console.log(`Bummer! You lost with ${wins} wins, ${losses} losses and ${ties} ties.`)
+
+    button.forEach((value) => {
+        value.removeEventListener('click', round)
+    })
 }
+
+// button.addEventListener('click', (e)=> {
+//     x = playRound(e.target.textContent.toLowerCase(),getComputerChoice());
+//     if (x.includes('win')) {
+//         wins++;
+//         rounds++;
+//     } else if(x.includes('lose')) {
+//         losses++;
+//         rounds++;
+//     } else if (x.includes('tie')) {
+//         ties++;
+//     }
+
+//     if (wins == 5) {
+//         endGame('win');
+//     } else if (losses == 5) {
+//         endGame('lose')
+//     }
+// }
+// )
+
+// function game() {
+//     let choice;
+//     let wins = 0, losses = 0, ties = 0;
+//     let rounds = 0;
+//     let x;
+//     while (rounds < 5) {
+//         choice = prompt('Choose between rock, paper and scissors.').toLowerCase()
+//         while (!(allowed.includes(choice))) {
+//             choice = prompt(`Please select only the allowed values, that is: ${allowed}`).toLowerCase();
+//         }
+//         console.log(x = playRound(choice, getComputerChoice()))
+//         if (x.includes('win')) {
+//             wins++;
+//             rounds++;
+//         } else if(x.includes('lose')) {
+//             losses++;
+//             rounds++;
+//         } else if (x.includes('tie')) {
+//             ties++;
+//         }
+//     }
+//     (wins > losses) ? console.log(`Yaaay!! You won with ${wins} wins, ${losses} losses and ${ties} ties.`) : console.log(`Bummer! You lost with ${wins} wins, ${losses} losses and ${ties} ties.`)
+// }
